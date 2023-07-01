8c6f359ece10d2bed3a7dddba3cb54c51994cb9d
KrishnaYamsani <21cs01019@iitbbs.ac.in>
2023-07-01 09:40:53 +0530

    React dev tools added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-if(process.env.NODE_ENV === "production") disableReactDevTools();
+// if(process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.render(<App />, document.getElementById("root"));
