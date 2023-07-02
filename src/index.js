f152acf799c321eaa4414802564e0a27cca874e1
KrishnaYamsani <21cs01019@iitbbs.ac.in>
2023-07-02 12:17:03 +0530

    Removed disable-react-develors npm package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
 
 // if(process.env.NODE_ENV === "production") disableReactDevTools();
 
